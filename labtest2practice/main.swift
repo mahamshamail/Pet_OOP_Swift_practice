9a79a5d81dbfcccbd55d5552ca80a138dc9ac3ab
maham <mahamshamail@gmail.com>
2020-11-05 19:00:39 -0500

    Initial Commit

---
+++ labtest2practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  labtest2practice
+//
+//  Created by Maham Shamail on 05/11/2020.
+//  Copyright © 2020 Maham Shamail. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
